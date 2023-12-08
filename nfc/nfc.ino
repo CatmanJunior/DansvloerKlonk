1c2d8ebced4d399eaecdbd6c5c7702b329c14349
CatmanJunior <rjm.thart@gmail.com>
2023-12-08 01:22:23 +0100

    Refactor code for improved performance and readability

--- nfc/nfc.ino
+++ nfc/nfc.ino
@@ -13,17 +13,17 @@
   GND     = GND
   3.3V    = 3.3V
 */
-const int RST_PIN   = 0;  // RST-PIN für RC522 - RFID - SPI - Modul GPIO5
-const int SS_PIN    = 15; // SDA-PIN für RC522 - RFID - SPI - Modul GPIO4
-const int LED_PIN   = 5;  //Pin which the leds are connected (GPIO5 = D1)
+const int RST_PIN = 0; // RST-PIN für RC522 - RFID - SPI - Modul GPIO5
+const int SS_PIN = 15; // SDA-PIN für RC522 - RFID - SPI - Modul GPIO4
+const int LED_PIN = 5; // Pin which the leds are connected (GPIO5 = D1)
 
-const int NUMPIXELS = 17; //Total number of Leds in each tile
-const int GROUPSIZE = 4;  //Size of the leds in each group
-const int SENSORTRESH = 50; //Treshhold for the data from piezo element
+const int NUMPIXELS = 17;   // Total number of Leds in each tile
+const int GROUPSIZE = 4;    // Size of the leds in each group
+const int SENSORTRESH = 50; // Treshhold for the data from piezo element
 const int NFCTIMEOUT = 500;
 
-const char *ssid =  "Ziggo78F5D45";     // change according to your Network - cannot be longer than 32 characters!
-const char *pass =  "Sx7phx8fnkeP"; // change according to your Network
+const char *ssid = "Ziggo78F5D45"; // change according to your Network - cannot be longer than 32 characters!
+const char *pass = "Sx7phx8fnkeP"; // change according to your Network
 const char *mqtt_server = "192.168.178.40";
 const char *NAME = "Tile0";
 const char *TOPIC = "t0";
@@ -42,139 +42,114 @@ char msg[50];
 
 int lastState = 0;
 int readData;
-MFRC522::StatusCode status; //variable to get card status
+MFRC522::StatusCode status; // variable to get card status
 
-byte buffer[18];  //data transfer buffer (16+2 bytes data+CRC)
+byte buffer[18]; // data transfer buffer (16+2 bytes data+CRC)
 byte size = sizeof(buffer);
 
 uint8_t pageAddr = 0x06;
 
-void setup() {
-  pixels.begin(); // This initializes the NeoPixel library.
+void setup()
+{
+  pixels.begin(); // Initialize the NeoPixel library.
   StartSequence();
 
-  Serial.begin(115200);    // Initialize serial communications
-  Serial.println(NAME);
+  Serial.begin(115200); // Initialize serial communications
 
-  SPI.begin();           // Init SPI bus
+  SPI.begin();        // Init SPI bus
+  mfrc522.PCD_Init(); // Init MFRC522
+  mfrc522.PCD_SetAntennaGain(mfrc522.RxGain_max);
 
-  mfrc522.PCD_Init();    // Init MFRC522
-  Serial.println(mfrc522.PCD_GetAntennaGain()); // gives me a 64 (00000010)
-  mfrc522.PCD_SetAntennaGain(112); // set to max (00001110)
-  Serial.println(mfrc522.PCD_GetAntennaGain()); //gives me the 112
-  memcpy(buffer, "2", 1);
-
-  WiFi.begin(ssid, pass);
-
+  setup_wifi();
   client.setServer(mqtt_server, 1883);
   client.setCallback(callback);
-
-  while ((WiFi.status() != WL_CONNECTED)) {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println(F("WiFi connected"));
-  ColorAll(0, 255, 0);
 }
 
 void loop() {
-  sensorValue = analogRead(A0);
+  maintainWiFiConnection();
+  maintainMQTTConnection();
 
+  if (mfrc522.PICC_IsNewCardPresent() && mfrc522.PICC_ReadCardSerial()) {
+    handleRFID();
+  }
+
+  processSensorData();
+
+  manageNFCReadings();
+
+  delay(10); // Short delay to stabilize loop execution
+}
+
+void maintainWiFiConnection() {
+  if (WiFi.status() != WL_CONNECTED) {
+    setup_wifi();
+  }
+}
+
+void maintainMQTTConnection() {
+  if (!client.connected()) {
+    connectMQTT();
+  }
+  client.loop();
+}
+
+void processSensorData() {
+  sensorValue = analogRead(A0);
   if (sensorValue >= SENSORTRESH) {
     sendSensor(sensorValue);
   }
+}
 
-  // Serial.println(sensorValue);
-  // && mfrc522.PICC_ReadCardSerial()
-  if ( mfrc522.PICC_IsNewCardPresent() || mfrc522.PICC_ReadCardSerial()) {
-    // Show some details of the PICC (that is: the tag/card)
-    //Serial.print(F("Card UID:"));
-    //dump_byte_array(mfrc522.uid.uidByte, mfrc522.uid.size);
-    //Serial.println();
-    // Read data ***************************************************
-    //Serial.println(F("Reading data ... "));
-    //data in 4 block is readed at once.
-    if (lastState == 0) {
-      status = (MFRC522::StatusCode) mfrc522.MIFARE_Read(pageAddr, buffer, &size);
-      if (status != MFRC522::STATUS_OK) {
-        Serial.print(F("MIFARE_Read() failed: "));
-        Serial.println(mfrc522.GetStatusCodeName(status));
-        return;
-      }
-
-      Serial.print(F("Readed data: "));
-      //Dump a byte array to Serial
-
-      Serial.write(buffer[0]);
-
-      Serial.println();
-      readData = buffer[0];
-      memset(buffer, 0, 18);
-    }
-    foundCard = true;
-
-
-  }
-
-  size = sizeof(buffer);
-
+void manageNFCReadings() {
   long now = millis();
   if (now - lastMsg > NFCTIMEOUT) {
     if (!foundCard && lastState != 0) {
       sendNull();
       lastState = 0;
     } else if (foundCard && lastState != 1) {
-      
       sendMsg(readData);
       lastState = 1;
-
     }
     foundCard = false;
     lastMsg = now;
-
   }
+}
 
-  if (!client.connected()) {
-    reconnect();
+void handleRFID() {
+  if (lastState == 0) {
+    status = (MFRC522::StatusCode)mfrc522.MIFARE_Read(pageAddr, buffer, &size);
+    if (status != MFRC522::STATUS_OK) {
+      Serial.print(F("MIFARE_Read() failed: "));
+      Serial.println(mfrc522.GetStatusCodeName(status));
+      return;
+    }
+
+    Serial.print(F("Read data: "));
+    Serial.write(buffer[0]);
+    Serial.println();
+
+    readData = buffer[0];
+    memset(buffer, 0, 18);
   }
-  client.loop();
-  delay(10);
-}
-
-void sendMsg(int ms) {
-  snprintf (msg, 75, "a%ld", ms - 48);
-  Serial.print("Publish message: ");
-  Serial.println(msg);
-  client.publish(TOPIC, msg);
+  foundCard = true;
 }
 
 
-void sendNull() {
-  snprintf (msg, 75, "a%ld", 0);
-  Serial.print("Publish message: ");
-  Serial.println(msg);
-  client.publish(TOPIC, msg);
-}
-
-void sendSensor(int ms) {
-  snprintf (msg, 75, "s%ld", ms);
-  Serial.print("Publish message: ");
-  Serial.println(msg);
-  client.publish(TOPIC, msg);
-}
-
-void reconnect() {
+void connectMQTT()
+{
   // Loop until we're reconnected
-  while (!client.connected()) {
+  while (!client.connected())
+  {
     Serial.print("Attempting MQTT connection...");
     // Attempt to connect
-    if (client.connect(NAME)) {
+    if (client.connect(NAME))
+    {
       Serial.println("connected");
-      // Once connected, publish an announcement...
-      client.publish("outTopic", "hello world");
-      // ... and resubscribe
-      client.subscribe(LEDTOPIC);
-    } else {
+      // Subscribe to topics here if needed
+      client.subscribe(TOPIC);
+    }
+    else
+    {
       Serial.print("failed, rc=");
       Serial.print(client.state());
       Serial.println(" try again in 5 seconds");
@@ -184,7 +159,54 @@ void reconnect() {
   }
 }
 
-void callback(char* topic, byte* payload, unsigned int length) {
+void setup_wifi()
+{
+  delay(10);
+  // Start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, pass);
+
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void sendMsg(int ms)
+{
+  snprintf(msg, 75, "a%ld", ms - 48);
+  Serial.print("Publish message: ");
+  Serial.println(msg);
+  client.publish(TOPIC, msg);
+}
+
+void sendNull()
+{
+  snprintf(msg, 75, "a%ld", 0);
+  Serial.print("Publish message: ");
+  Serial.println(msg);
+  client.publish(TOPIC, msg);
+}
+
+void sendSensor(int ms)
+{
+  snprintf(msg, 75, "s%ld", ms);
+  Serial.print("Publish message: ");
+  Serial.println(msg);
+  client.publish(TOPIC, msg);
+}
+
+void callback(char *topic, byte *payload, unsigned int length)
+{
   Serial.print("Message arrived [");
   Serial.print(topic);
   Serial.print("] ");
@@ -194,38 +216,41 @@ void callback(char* topic, byte* payload, unsigned int length) {
   int g = (payload[3] - '0') * 100 + (payload[4] - '0') * 10 + (payload[5] - '0');
   int b = (payload[6] - '0') * 100 + (payload[7] - '0') * 10 + (payload[8] - '0');
 
-  int t = payload[9] - '0'; // Assuming payload contains ASCII characters
+  int t = payload[9] - '0';                               // Assuming payload contains ASCII characters
   int d = (payload[10] - '0') * 10 + (payload[11] - '0'); // Assuming two digits
 
   Serial.print(t);
   Serial.println(d);
 
-  switch (t) {
-    case 1:
-      ColorAll(r, g, b);
-      break;
-    case 2:
-      ColorPer(r, g, b, 50);
-      break;
-    case 3:
-      ColorGroup(r, g, b, d);
-      break;
-    case 4:
-      ColorLed(r, g, b, d);
-      break;
+  switch (t)
+  {
+  case 1:
+    ColorAll(r, g, b);
+    break;
+  case 2:
+    ColorPer(r, g, b, 50);
+    break;
+  case 3:
+    ColorGroup(r, g, b, d);
+    break;
+  case 4:
+    ColorLed(r, g, b, d);
+    break;
   }
 }
 
-
 // Helper routine to dump a byte array as hex values to Serial
-void dump_byte_array(byte *buffer, byte bufferSize) {
-  for (byte i = 0; i < bufferSize; i++) {
+void dump_byte_array(byte *buffer, byte bufferSize)
+{
+  for (byte i = 0; i < bufferSize; i++)
+  {
     Serial.print(buffer[i] < 0x10 ? " 0" : " ");
     Serial.print(buffer[i], HEX);
   }
 }
 
-void StartSequence() {
+void StartSequence()
+{
   ColorPer(255, 0, 0, 100);
   delay(500);
   ColorPer(0, 255, 0, 100);
@@ -235,34 +260,40 @@ void StartSequence() {
   ColorAll(255, 0, 0);
 }
 
-
-void ColorAll(int r, int g, int b) {
-  for (int i = 0; i < NUMPIXELS; i++) {
+void ColorAll(int r, int g, int b)
+{
+  for (int i = 0; i < NUMPIXELS; i++)
+  {
     // pixels.Color takes RGB values, from 0,0,0 up to 255,255,255
     pixels.setPixelColor(i, pixels.Color(r, g, b)); // Moderately bright green color.
   }
   pixels.show(); // This sends the updated pixel color to the hardware.
 }
 
-void ColorLed(int r, int g, int b, int l) {
+void ColorLed(int r, int g, int b, int l)
+{
   pixels.setPixelColor(l, pixels.Color(r, g, b)); // Moderately bright green color.
-  pixels.show(); // This sends the updated pixel color to the hardware.
+  pixels.show();                                  // This sends the updated pixel color to the hardware.
 }
 
-void ColorPer(int r, int g, int b, int del) {
-  for (int i = 0; i < NUMPIXELS; i++) {
+void ColorPer(int r, int g, int b, int del)
+{
+  for (int i = 0; i < NUMPIXELS; i++)
+  {
     // pixels.Color takes RGB values, from 0,0,0 up to 255,255,255
     pixels.setPixelColor(i, pixels.Color(r, g, b)); // Moderately bright green color.
-    pixels.show(); // This sends the updated pixel color to the hardware.
+    pixels.show();                                  // This sends the updated pixel color to the hardware.
     delay(del);
   }
 }
 
-void ColorGroup(int r, int g, int b, int group) {
-  for (int i = GROUPSIZE * group; i < GROUPSIZE * group + GROUPSIZE; i++) {
+void ColorGroup(int r, int g, int b, int group)
+{
+  for (int i = GROUPSIZE * group; i < GROUPSIZE * group + GROUPSIZE; i++)
+  {
     // pixels.Color takes RGB values, from 0,0,0 up to 255,255,255
     pixels.setPixelColor(i, pixels.Color(r, g, b)); // Moderately bright green color.
-    pixels.show(); // This sends the updated pixel color to the hardware.
+    pixels.show();                                  // This sends the updated pixel color to the hardware.
     delay(100);
   }
 }
